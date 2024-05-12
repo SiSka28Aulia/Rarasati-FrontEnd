47b7bbde527e19fa174653dd3d02a0527e302b61
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2024-05-12 12:39:34 +0700

    update

--- Paws-Connect/src/Components/Hero.jsx
+++ Paws-Connect/src/Components/Hero.jsx
@@ -1,38 +1,53 @@
 import React from "react";
-import { catHome1 } from "../assets";
+import styles from "../style";
+import { catHome1, catHome2 } from "../assets";
 import Adopsi from "./Adopsi";
 
 const Hero = () => {
   return (
     <div>
-      <section id="home">
-        <div>
-          <div className="flex flex-row justify-between items-center w-full">
-            <div className="text-sky-950 text-[70px] font-bold font-['Satoshi'] [font-family:'Satoshi',Helvetica] leading-[105px]">
-              Dapatkan Anggota
+      <section
+        id="home"
+        className={`flex md:flex-row flex-col${styles.paddingY} ${styles.flexCenter}`}
+      >
+        <div
+          className={`flex-1 flex ${styles.flexStart} flex-col md:mr-10 mr-10 md:mt-0 mt-10 relative`}
+        >
+          <div className="flex flex-row items-center py-[18px] px-[6px] mb-8 mt-20">
+            {/* Bagian Kiri */}
+            <div className="flex flex-1 flex-col items-start justify-center">
+              <div className="text-sky-950 text-[70px] font-bold font-['Satoshi'] [font-family:'Satoshi',Helvetica] leading-[105px]">
+                Dapatkan Anggota
+              </div>
+              <p className="text-sky-950 text-[70px] font-bold font-['Satoshi'] [font-family:'Satoshi',Helvetica] leading-[105px]">
+                Keluarga Bulu Baru <br />
+                Anda Hari Ini
+              </p>
+              <p className="text-sky-950 text-xl font-light [font-family:'Satoshi',Helvetica] leading-[30px] mt-5">
+                Para Kucing yang Menunggu untuk Menjadi Bagian dari Rumah Anda!
+                Temukan Sahabat Sejati Anda Hari Ini di Website Adopsi Kucing
+                Kami
+              </p>
+              <div className="mt-10">
+                <Adopsi />
+              </div>
             </div>
-            <div className="ss:flex hidden md:mr-4 mr-0">
-              <Adopsi />
+
+            {/* Bagian Kanan */}
+            <div className="flex-1 flex justify-center items-center md:mr-10 mr-0 md:mt-0 mt-10 relative">
+              <img
+                src={catHome1}
+                alt="cathome1"
+                className="w-[100%] h-[100%] relative z-[5] mb-[-5.50px]"
+              />
+              <br />
+              <img
+                src={catHome2}
+                alt="cathome2"
+                className="w-[100%] h-[100%] relative z-[5] mt-[-5.50px]"
+              />
             </div>
           </div>
-          <div className="text-sky-950 text-[70px] font-bold font-['Satoshi'] [font-family:'Satoshi',Helvetica] leading-[105px]">
-            Keluarga Bulu Baru <br />
-            Anda Hari Ini
-          </div>
-          <p className="w-[623px] text-sky-950 text-xl font-light [font-family:'Satoshi',Helvetica] leading-[30px]">
-            Para Kucing yang Menunggu untuk Menjadi Bagian dari Rumah Anda!
-            Temukan Sahabat Sejati Anda Hari Ini di Website Adopsi Kucing Kami
-          </p>
-        </div>
-        <div>
-          <img
-            src={catHome1}
-            alt="cathome1"
-            className="relative z-[5] w-[50%] h-[50%] bottom-50 right-90"
-          />
-        </div>
-        <div>
-          <Adopsi />
         </div>
       </section>
     </div>
--- Paws-Connect/src/Components/Navbar.jsx
+++ Paws-Connect/src/Components/Navbar.jsx
@@ -4,48 +4,96 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="nav">
-        <div className="box-logo">
-          <div className="group-logo">
-            <div className="icon-logo">
-              <img src={logo1} alt="logo1" className="logo1" />
-              <img src={logo2} alt="logo2" className="logo2" />
-            </div>
-            <Link to="/" className="logo">
-              Paws Connect
+    <div className="border-b-2 shadow-md fixed w-full bg-white md:h-80px] ">
+      <div className="md:flex space-x-6 hidden items-center flex-col justify-center md:flex-row">
+        <div className="flex justify-between items-center">
+          <div className="flex">
+            <Link to="/">
+              <img
+                src={logo1}
+                alt="logo"
+                className="w-[100%]  relative left-12 top-16.5 origin-top-left rotate-[-2.44deg]"
+              />
+            </Link>
+            <Link to="/">
+              <img
+                src={logo2}
+                alt="logo"
+                className="w-[100%]  relative left-12 top-16.5 origin-top-left rotate-[15deg]"
+              />
+            </Link>
+            <Link to="/">
+              <div className="w-96 h-24 text-[#A367B1] text-2xl italic font-bold font-['Inter'] leading-[40px] relative left-5 top-7">
+                Paw Connect
+              </div>
+            </Link>
+          </div>
+          <div
+            className="md:flex space-x-6  items-center flex-col
+          justify-center md:flex-row"
+          >
+            <Link to="/">
+              <div
+                className="text-sky-950 text-[15px] font-light
+              [font-family:'Satoshi',Helvetica] leading-[40px]"
+              >
+                Beranda
+              </div>
+            </Link>
+            <Link to="/adopsi">
+              <div
+                className="text-sky-950 text-[15px] font-light
+              [font-family:'Satoshi',Helvetica] leading-[40px]"
+              >
+                Adopsi
+              </div>
+            </Link>
+            <Link to="/artikel">
+              <div
+                className="text-sky-950 text-[15px] font-light
+              [font-family:'Satoshi',Helvetica] leading-[40px]"
+              >
+                Artikel
+              </div>
+            </Link>
+            <Link to="/kontak">
+              <div
+                className="text-sky-950 text-[15px] font-light
+              [font-family:'Satoshi',Helvetica] leading-[40px]"
+              >
+                Kontak
+              </div>
+            </Link>
+            <Link to="/komunitas">
+              <div
+                className="text-sky-950 text-[15px] font-light
+              [font-family:'Satoshi',Helvetica] leading-[40px]"
+              >
+                Komunitas
+              </div>
             </Link>
           </div>
         </div>
-        <div className="navbar">
-          <Link to="/" className="nav-link">
-            Beranda
-          </Link>
-          <Link to="/adopsi" className="nav-link">
-            Adopsi
-          </Link>
-          <Link to="/artikel" className="nav-link">
-            Artikel
-          </Link>
-          <Link to="/kontak" className="nav-link">
-            Kontak
-          </Link>
-          <Link to="/komunitas" className="nav-link">
-            Komunitas
-          </Link>
-        </div>
-        <div className="search-box">
-          <div className="search-icon">
-            <img
-              src="./src/assets/search.svg"
-              alt="search"
-              className="search-png"
-            />
-            <input type="text" placeholder="Cari" />
+        <div className="flex p-4 ">
+          <div className="relative ">
+            <div className=" w-[250px] h-[32px] border-2  rounded-[30px] border-[#A367B1]  text-[15px] font-light [font-family:'Satoshi',Helvetica] leading-[40px] mx-10">
+              <img
+                src={search}
+                alt="search"
+                className="w-[17px] h-[17px] relative top-1 right-70 left-1"
+              />
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="Cari"
+                  className="border-0 bg-transparent top-1  border rounded-md placeholder:text-[#A367B1] text-center w-[200px] h-[17px] text-sky-950 text-[15px] font-light [font-family:'Satoshi',Helvetica] leading-[40px]"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
