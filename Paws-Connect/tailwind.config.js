d39fe7f5ce898abf4b4fd8a5f3feeebc57736777
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2024-06-21 10:27:55 +0700

    docd : add daisyui config

--- Paws-Connect/tailwind.config.js
+++ Paws-Connect/tailwind.config.js
@@ -1,72 +1,55 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    // screens: {
-    //   xs: "480px",
-    //   ss: "620px",
-    //   sm: "768px",
-    //   md: "1060px",
-    //   lg: "1200px",
-    //   xl: "1700px",
-    // },
-
-    //Font Satoshi
-    fontFamily: {
-      fontFamily: "Satoshi-Light",
-      src: "url('../fonts/Satoshi-Light.woff2') format('woff2'), url('../fonts/Satoshi-Light.woff') format('woff'), url('../fonts/Satoshi-Light.ttf') format('truetype')",
-      fontWeight: "300",
-      fontStyle: "normal",
-    },
-
-    fontFamily: {
-      fontFamily: "Satoshi-Regular",
-      src: "url('../fonts/Satoshi-Regular.woff2') format('woff2'), url('../fonts/Satoshi-Regular.woff') format('woff'), url('../fonts/Satoshi-Regular.ttf') format('truetype')",
-      fontWeight: "400",
-      fontStyle: "normal",
-    },
-
-    fontFamily: {
-      fontFamily: "Satoshi-Medium",
-      src: "url('../fonts/Satoshi-Medium.woff2') format('woff2'), url('../fonts/Satoshi-Medium.woff') format('woff'), url('../fonts/Satoshi-Medium.ttf') format('truetype')",
-      fontWeight: "500",
-      fontStyle: "normal",
-    },
-
-    fontFamily: {
-      fontFamily: "Satoshi-Bold",
-      src: "url('../fonts/Satoshi-Bold.woff2') format('woff2'), url('../fonts/Satoshi-Bold.woff') format('woff'), url('../fonts/Satoshi-Bold.ttf') format('truetype')",
-      fontWeight: "700",
-      fontStyle: "normal",
-    },
-
-    fontFamily: {
-      fontFamily: "Satoshi-BoldItalic",
-      src: "url('../fonts/Satoshi-BoldItalic.woff2') format('woff2'), url('../fonts/Satoshi-BoldItalic.woff') format('woff'), url('../fonts/Satoshi-BoldItalic.ttf') format('truetype')",
-      fontWeight: "700",
-      fontStyle: "italic",
-    },
-
-    //Font Inter
-    fontFamily: {
-      fontFamily: "Inter",
-      src: "url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')",
-      fontWeight: "100..900",
-      fontStyle: "normal",
-    },
-
-    colors: {
-      primary: "#a367b1",
-      secondary: "#0C2D48",
-      greyLighter: "#f8f8f8",
-      greyLight: "#d9d9d9",
-      white: "#ffffff",
-      cyan: "#274472",
-      violet: "#6664D6",
-      purple: "#AFA4C8",
-      pink: "#D7B3D7",
+    extend: {
+      width: {
+        "480px": "490px",
+      },
+      height: {
+        "613px": "613px",
+      },
+      fontFamily: {
+        "Satoshi-Light": ["Satoshi-Light", ...fontFamily.sans],
+        "Satoshi-Regular": ["Satoshi-Regular", ...fontFamily.sans],
+        "Satoshi-Medium": ["Satoshi-Medium", ...fontFamily.sans],
+        "Satoshi-Bold": ["Satoshi-Bold", ...fontFamily.sans],
+        "Satoshi-BoldItalic": ["Satoshi-BoldItalic", ...fontFamily.sans],
+        Inter: ["Inter", ...fontFamily.sans],
+      },
+      colors: {
+        primary: "#a367b1",
+        secondary: "#0C2D48",
+        greyLighter: "#f8f8f8",
+        greyLight: "#d9d9d9",
+        white: "#ffffff",
+        cyan: "#274472",
+        violet: "#6664D6",
+        purple: "#AFA4C8",
+        pink: "#D7B3D7",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a367b1",
+          secondary: "#0C2D48",
+          greyLighter: "#f8f8f8",
+          greyLight: "#d9d9d9",
+          white: "#ffffff",
+          cyan: "#274472",
+          violet: "#6664D6",
+          purple: "#AFA4C8",
+          pink: "#D7B3D7",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 };
