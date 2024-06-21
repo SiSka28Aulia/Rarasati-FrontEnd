f7df1f4f31a3c8fe4d06cd2b654519f5a14ed995
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2024-06-21 09:42:14 +0700

    style: update artikel component

--- Paws-Connect/src/Components/Artikel.jsx
+++ Paws-Connect/src/Components/Artikel.jsx
@@ -1,20 +1,32 @@
-import React from "react";
-import styles from "../style";
+import React, { useState } from "react";
 import ArtikelItem from "./ArtikelItem";
 import Button from "./Button";
 
 const Artikel = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <section className="">
         <div className="flex items-center justify-between mx-20 mt-10 mb-10">
-          <h1 className="text-cyan text-3xl font-bold font-Satoshi-Regular ">
+          <h1 className="text-cyan text-3xl font-bold font-Satoshi-Regular">
             Artikel Terbaru Kami
           </h1>
-          <Button button="Selengkapnya" />
+          <Button variant="primary" onClick={handleOpenModal}>
+            Selengkapnya
+          </Button>
         </div>
         <ArtikelItem />
       </section>
+      {isModalOpen && <Modal onClose={handleCloseModal} />}
     </>
   );
 };
