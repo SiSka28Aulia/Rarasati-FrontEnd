fea4ea9297f205d8d915d98770bd6b437842c746
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2024-06-21 08:31:39 +0700

    feat : add register page

--- Paws-Connect/src/Auth/Register.jsx
+++ Paws-Connect/src/Auth/Register.jsx
@@ -1,77 +1,175 @@
-import React from "react";
-import UseForm from "../hooks/UseForm";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { login } from "../assets/index.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { logobaru } from "../assets/index.js";
+
 const Register = () => {
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
   const {
     register,
-
+    handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
 
+  const password = watch("password");
+  const confirmPassword = watch("confirmPassword");
 
-  const { handleChange, handleSubmit, values } = UseForm(() => {
-    console.log("Form submitted");
-  });
+  const handleRegister = async (data) => {
+    const { firstName, lastName, email, password, confirmPassword } = data;
+
+    if (password !== confirmPassword) {
+      setError("Password dan Konfirmasi Password tidak cocok");
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:8000/auth-register", {
+        nama_depan: firstName,
+        nama_belakang: lastName,
+        email,
+        password,
+        confirmPassword,
+      });
+      localStorage.setItem("token", response.data.token);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response?.data?.message || "Email sudah terdaftar");
+    }
+  };
 
   return (
-    <>
-      <div className="min-h-screen flex items-center justify-center ">
-        <div className="w-480px h-613px flex shadow-lg rounded-lg bg-white overflow-hidden">
-          <div className="w-1/2 bg-cover bg-center bg-pink" style={{ backgroundImage: `url(${login})` }}>
-            <div className="p-10">
-              <h1 className="text-3xl font-bold text-white text-center">Selamat Datang</h1>
-
-              <div className="flex mt-4">
-                <a href="#" className="text-white text-2xl mr-2"><i className="fa fa-facebook"></i></a>
-                <a href="#" className="text-white text-2xl"><i className="fa fa-twitter"></i></a>
-              </div>
-            </div>
+    <form onSubmit={handleSubmit(handleRegister)}>
+      <div className="min-h-screen flex items-center justify-center bg-primary">
+        <div className="w-96 p-8 bg-white shadow-lg rounded-3xl">
+          <div className="flex items-center justify-center mb-2">
+            <img src={logobaru} alt="Logo" className="w-48" />
           </div>
-          <div className="w-1/2 p-8">
-            <h5 className="text-xl text-center font-bold text-black mt-20 mb-8">Mulai mendaftar</h5>
-
-            <form onSubmit={handleSubmit}>
-              <div className="mt-4">
-                <label htmlFor="firstName">First Name</label>
-                <input
-                  type="text"
-                  id="firstName"
-                  value={values.username || ""}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
-                  type="text"
-                  placeholder="Username"
-                />
-              </div>
-              <div className="mt-4">
-                <input
-                  name="password"
-                  value={values.password || ""}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
-                  type="password"
-                  placeholder="Password"
-                />
-              </div>
-              <p className="text-sm text-secondary font-bold mt-1">Sudah punya akun?
-                <Link to="/login">
-                  <a href="#" className="font-light text-primary underline ">
-
-                    Masuk</a>
-                </Link>
-              </p>
-              <button className="w-full mt-4 bg-primary text-white py-2 rounded-lg font-semibold hover:bg-purple">
-                Daftar
+          <h5 className="text-xl text-center font-semibold text-black mt-6 mb-8">
+            Mulai mendaftar
+          </h5>
+          <div className="mt-4">
+            <label htmlFor="firstName">
+              <input
+                type="text"
+                id="firstName"
+                placeholder="First Name"
+                className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
+                {...register("firstName", {
+                  required: "First name is required",
+                })}
+              />
+              {errors.firstName && (
+                <span className="text-red-500 text-xs font-light mt-1">
+                  {errors.firstName.message}
+                </span>
+              )}
+            </label>
+          </div>
+          <div className="mt-4">
+            <label htmlFor="lastName">
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name"
+                className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
+                {...register("lastName", { required: "Last name is required" })}
+              />
+              {errors.lastName && (
+                <span className="text-red-500 text-xs font-light mt-1">
+                  {errors.lastName.message}
+                </span>
+              )}
+            </label>
+          </div>
+          <div className="mt-4 relative">
+            <label htmlFor="email">
+              <input
+                type="text"
+                id="email"
+                placeholder="Email"
+                className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="text-red-500 text-xs font-light mt-1">
+                  {errors.email.message}
+                </span>
+              )}
+            </label>
+          </div>
+          <div className="mt-4 relative">
+            <label htmlFor="password">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Password"
+                className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters long",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="text-red-500 text-xs font-light mt-1">
+                  {errors.password.message}
+                </span>
+              )}
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-0 right-0 px-3 flex items-center text-sm text-gray-600"
+              >
+                {showPassword ? "Hide" : "Show"}
               </button>
-            </form>
+            </label>
           </div>
+          <div className="mt-4 relative">
+            <label htmlFor="confirmPassword">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                className="w-full px-4 py-2 border rounded-lg text-sm focus:outline-none focus:border-primary"
+                {...register("confirmPassword", {
+                  required: "Confirm Password is required",
+                })}
+              />
+              {errors.confirmPassword && (
+                <span className="text-red-500 text-xs font-light mt-1">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+            </label>
+          </div>
+
+          <p className="text-sm text-secondary font-bold mt-1 ">
+            Sudah punya akun?
+            <Link to="/login">
+              <span className="font-light text-primary underline ml-2">
+                Masuk
+              </span>
+            </Link>
+          </p>
+          <button
+            type="submit"
+            className="w-full mt-4 bg-primary text-white py-2 rounded-lg font-semibold hover:bg-purple"
+          >
+            Daftar
+          </button>
+          {error && (
+            <div className="text-red-500 text-xs font-light mt-1">{error}</div>
+          )}
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
 export default Register;
-
