ae4e48a6c484dcc5cc04847bfdc046db907809ad
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2024-06-21 09:16:00 +0700

    style : update navbar

--- Paws-Connect/src/Components/Navbar.jsx
+++ Paws-Connect/src/Components/Navbar.jsx
@@ -1,104 +1,65 @@
 import React from "react";
-import { logo1, logo2, search } from "../assets";
+import { logobaru } from "../assets";
 import styles from "../style";
 import { Link } from "react-router-dom";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="md:flex space-x-6 hidden items-center flex-col justify-center md:flex-row">
-        <div className="flex justify-between items-center">
-          <div className="flex">
-            <Link to="/">
-              <img
-                src={logo2}
-                alt="logo"
-                className="w-[100%]  relative left-16 top-16.5 origin-top-left "
-              />
-            </Link>
-            <Link to="/">
-              <img
-                src={logo1}
-                alt="logo"
-                className="w-[100%]  relative left-12 -top-2 origin-top-left "
-              />
-            </Link>
-            <Link to="/">
-              <div
-                className="w-96 h-24 text-primary text-2xl  font-bold font-weight-700 italic font-Inter leading-[40px] md:flex space-x-6  items-start flex-col
-          justify-center"
-              >
-                Paw Connect
-              </div>
-            </Link>
-          </div>
-          <div
-            className="md:flex space-x-6  items-center flex-col
-          justify-center md:flex-row"
-          >
-            <Link to="/">
-              <div
-                className="text-sky-950 text-base font-light
-              font-Satoshi-Light leading-[40px]"
-              >
-                Beranda
-              </div>
-            </Link>
-            <Link to="/adopsi">
-              <div
-                className="text-sky-950 text-base font-light
-              font-Satoshi-Light leading-[40px]"
-              >
-                Adopsi
-              </div>
-            </Link>
-            <Link to="/artikel">
-              <div
-                className="text-sky-950 text-base font-light
-              font-Satoshi-Light leading-[40px]"
-              >
-                Artikel
-              </div>
-            </Link>
-            <Link to="/kontak">
-              <div
-                className="text-sky-950 text-base font-light
-              font-Satoshi-Light leading-[40px]"
-              >
-                Kontak
-              </div>
-            </Link>
-            <Link to="/komunitas">
-              <div
-                className="text-sky-950 text-base font-light
-              font-Satoshi-Light leading-[40px]"
-              >
-                Komunitas
-              </div>
-            </Link>
-          </div>
+    <div className="md:flex space-x-6 hidden items-center justify-center flex-col md:flex-row mt-2">
+      <div className="flex justify-between items-center w-full px-10">
+        <div className="flex items-center">
+          <Link to="/">
+            <img src={logobaru} alt="logo" className="w-20 mb-6" />
+          </Link>
         </div>
-        <div className="flex p-4 ">
-          <div className="relative ">
-            <div
-              className={` ${styles.flexCenter}  w-[270px] h-[32px] border-2  rounded-[30px] border-primary text-base font-light font-Satoshi-Light leading-[40px] mx-10`}
-            >
-              <img
-                src={search}
-                alt="search"
-                className="w-[17px] h-[17px] relative "
-              />
-
-              <input
-                type="text"
-                placeholder="Cari"
-                className="border-0 bg-transparent outline-0 rounded-md placeholder:text-primary text-center w-[200px] h-[17px] text-sky-950 text-base font-light font-Satoshi-Light leading-[40px]"
-              />
+        <div className="flex space-x-6">
+          <Link to="/">
+            <div className="text-sky-950 text-base font-light font-Satoshi-Light leading-[40px]">
+              Beranda
             </div>
-          </div>
+          </Link>
+          <Link to="/adopsi">
+            <div className="text-sky-950 text-base font-light font-Satoshi-Light leading-[40px]">
+              Adopsi
+            </div>
+          </Link>
+          <Link to="/artikel">
+            <div className="text-sky-950 text-base font-light font-Satoshi-Light leading-[40px]">
+              Artikel
+            </div>
+          </Link>
+          <Link to="/kontak">
+            <div className="text-sky-950 text-base font-light font-Satoshi-Light leading-[40px]">
+              Kontak
+            </div>
+          </Link>
+          <Link to="/komunitas">
+            <div className="text-sky-950 text-base font-light font-Satoshi-Light leading-[40px]">
+              Komunitas
+            </div>
+          </Link>
+        </div>
+        <div className="relative flex items-center space-x-4">
+          <label className="input input-bordered input-primary flex items-center gap-2 border-b-primary h-[30px]">
+            <input type="text" className="grow" placeholder="Cari..." />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70 text-primary"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+          <UserMenu />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
