7b8cb90a0ad6e514b7580e0fb93831a7c813ef70
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2024-06-22 23:03:24 +0700

    style : use children

--- Paws-Connect/src/Components/Button.jsx
+++ Paws-Connect/src/Components/Button.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 
-const Button = (props) => {
-  // const [more, setMore] = useState("Selengkapnya");
-  // const [adopt, setAdopt] = useState("Adopsi");
+const Button = ({ children }) => {
   return (
     <>
       <div className="flex w-[188px] h-[62px] items-center justify-center gap-[10px] p-[10px] relative bg-primary rounded-[15px]">
         <button className="relative w-fit mt-[-2.50px] mb-[-0.50px]  font-Satoshi-Regular text-3xl  text-white tracking-[-0.69px] leading-[34px] whitespace-nowrap">
-          {props.button}
+          {children}
         </button>
       </div>
     </>
